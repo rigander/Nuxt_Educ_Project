5dbe8c3cd196f218d7145435ba69f6eaf2bb22a4
Andrey Komov <andrewkomov@gmail.com>
2023-10-11 00:42:21 +0300

    middleware_2

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,5 +1,12 @@
 export default defineNuxtRouteMiddleware(
     (to, from) => {
+        const isLoggedIn = false;
+        // to is the destination
         console.log(to);
-        console.log(from);
-    })
+        if (isLoggedIn) {
+            // redirect to the page we want to go
+            return navigateTo(to.fullPath);
+        }
+        // redirect to a login page
+        return navigateTo("/login");
+    });
