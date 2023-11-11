00b4bcb2cb182cbf762016dfbc3749b1fef52c3f
Andrey Komov <andrewkomov@gmail.com>
2023-11-11 23:01:53 +0200

    seo

---
+++ pages/meta.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const title = ref("this is my dynamic title");
+</script>
+
+<template>
+    <div>
+        <Head>
+            <Title>This is my title</Title>
+            <Meta name="description" :content="title"/>
+            <Style type="text/css" children="body { background-color: green; }"/>
+        </Head>
+        Homepage
+    </div>
+</template>
